642151e3844c0bd8966bfb38f92381741c04e702
Miles <git@miles.tw>
2024-12-30 01:37:13 +0800

    extractor

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,16 @@
                 ~color|.25s|ease,background|.25s|ease
                 bg:lime-30:hover
                 fg:lime-80:active
-                fg:pink-40!:hover">
+                fg:pink-40!:hover
+                {fg:red;b:1|solid|red;f:24}:hover">
         can be correctly parsed
     </div>
 
     <div class="content:'hello-world'::after">
-        unable to be parsed correctly, likely due to the presence of the ' symbol
+        ::after
     </div>
 
-    <div class="fg:red!">
+    <div class="f:64!">
         unable to be parsed correctly, likely due to ending with the ! symbol
-    </div>
-
-    <div class="{fg:red;b:1|solid|red}:hover">
-        unable to be parsed correctly, likely due to containing the symbols {, }, or ;
     </div>
 </template>
--- uno.config.ts
+++ uno.config.ts
@@ -1,15 +1,62 @@
-import { defineConfig } from 'unocss'
+import { defineConfig, ExtractorContext } from 'unocss'
 import { generateCSS, MasterCSS } from '@master/css'
 
-const masterCss = new MasterCSS()
+const spaceRE = /\s+/g
+const symbolRE = /['"`;{}:=]+/g
+
+const findCompleteString = (content: string) => {
+    const completeStrings = content?.match(/((?<!\\)["'`])(?:\\\1|(?:(?!\1))[\S\s])*(?<!\\)\1/g)
+    return completeStrings
+}
+
+const replaceCompleteString = (content: string, completeStrings: string[] | null) => {
+    completeStrings?.forEach((completeString, index) => {
+        content = content.replace(completeString, `COMPLETE-STRING--${index}--`)
+    })
+    return content
+}
 
 export default defineConfig({
     rules: [
         [
-            /[\w\W]+/,
-            ([masterCssClass], { }) => {
-                return generateCSS([masterCssClass], masterCss)
+            /.+/,
+            ([classname], { }) => {
+                return generateCSS([classname])
             }
         ]
+    ],
+    extractors: [
+        {
+            name: 'mastercss',
+            extract: ({ code }: ExtractorContext) => {
+                const result = new Set<string>()
+
+                const process = (content?: string) => {
+                    if (!content) return
+                    const completeStrings = findCompleteString(content)
+                    const code = replaceCompleteString(content, completeStrings)
+                    const splitSpace = code.split(spaceRE)
+                    splitSpace.forEach((splitSpaceBlock) => {
+                        if (splitSpaceBlock.includes('COMPLETE-STRING--')) {
+                            const index = parseInt(splitSpaceBlock.split('COMPLETE-STRING--')[1].split('--')[0])
+                            const origin = completeStrings?.[index] ?? ''
+                            result.add(splitSpaceBlock.replace(`COMPLETE-STRING--${index}--`, origin))
+                            const trimQuotation = origin.slice(1, -1)
+                            process(trimQuotation)
+                            return
+                        }
+                        result.add(splitSpaceBlock)
+                        const splitSymbol = splitSpaceBlock.split(symbolRE)
+                        splitSymbol.forEach((splitSymbolBlock) => {
+                            result.add(splitSymbolBlock)
+                        })
+                    })
+
+                }
+
+                process(code)
+                return result
+            }
+        }
     ]
 })
